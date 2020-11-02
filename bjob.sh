829682666a4ce0689a0337395640349560ff2786
Wichayaporn Wongkamjan <c828w084@class00.umiacs.umd.edu>
2020-11-01 21:53:56 -0500

    new run for BeamRider

--- bjob.sh
+++ bjob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=BeamRider
+#SBATCH --job-name=Beam&Breakout
 #SBATCH --partition=class
 #SBATCH --qos=default
 #SBATCH --gres=gpu:1
-#SBATCH --mem=1gb
+#SBATCH --mem=2gb
 #SBATCH --time=23:00:00
 
 module load cuda/10.1.243
@@ -11,4 +11,6 @@ module load cudnn/v7.6.5
 
 srun -N 1 --pty --mem=1gb bash
 python3 atari_ddqn.py --train --env BeamRiderNoFrameskip-v4
+srun -N 1 --mem=1gb --pty bash
+python3 atari_ddqn.py --train --env BreakoutNoFrameskip-v4
 wait
